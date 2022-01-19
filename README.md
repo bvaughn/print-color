92c9a9b4f0cda980f762c77762ab7c512df7dd28
Brian Vaughn <bvaughn@fb.com>
2022-01-18 19:23:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# print-color
+Helper utility for printing colored text in Node or the Browser.
+
+This library was inspired by `chalk`. I wrote it because I often want to bounce between Node (Jest) and the browser when debugging a problem, and `chalk` only supports Node.
+
+## Usage example
+```js
+import {bold, red} from 'print-color';
+
+red('This text will be red.');
+red.bold('This text will be red and bold');
+bold.red('This text will also be red and bold');
+```
+
+## Environments
+This utility works in the browser:
+<img width="114" alt="Screen Shot of print-color demo in Node terminal" src="https://user-images.githubusercontent.com/29597/150039892-793751cb-eb80-49f1-8c9e-7f3a2450bff4.png">
+
+It also works in Node:
+<img width="104" alt="Screen Shot of print-color demo in Chrome console" src="https://user-images.githubusercontent.com/29597/150039894-99155523-7acc-4e37-8651-2193dfd687fa.png">
---
+++ index.js
@@ -0,0 +1,191 @@
+const COLORS = {
+  background: {
+    bgBlack: {
+      browser: [0, 0, 0],
+      node: '\x1b[40m',
+    },
+    bgRed: {
+      browser: [255, 0, 0],
+      node: '\x1b[41m',
+    },
+    bgGreen: {
+      browser: [0, 255, 0],
+      node: '\x1b[42m',
+    },
+    bgYellow: {
+      browser: [255, 255, 0],
+      node: '\x1b[43m',
+    },
+    bgBlue: {
+      browser: [0, 0, 255],
+      node: '\x1b[44m',
+    },
+    bgMagenta: {
+      browser: [255, 0, 255],
+      node: '\x1b[45m',
+    },
+    bgCyan: {
+      browser: [0, 255, 255],
+      node: '\x1b[46m',
+    },
+    bgWhite: {
+      browser: [255, 255, 255],
+      node: '\x1b[47m',
+    },
+  },
+  foreground: {
+    black: {
+      browser: [0, 0, 0],
+      node: '\x1b[30m',
+    },
+    red: {
+      browser: [255, 0, 0],
+      node: '\x1b[31m',
+    },
+    green: {
+      browser: [0, 255, 0],
+      node: '\x1b[32m',
+    },
+    yellow: {
+      browser: [255, 255, 0],
+      node: '\x1b[33m',
+    },
+    blue: {
+      browser: [0, 0, 255],
+      node: '\x1b[34m',
+    },
+    magenta: {
+      browser: [255, 0, 255],
+      node: '\x1b[35m',
+    },
+    cyan: {
+      browser: [0, 255, 255],
+      node: '\x1b[36m',
+    },
+    white: {
+      browser: [255, 255, 255],
+      node: '\x1b[37m',
+    },
+  },
+};
+
+const NODE_RESET = '\x1b[0m';
+const NODE_BOLD = '\x1b[1m';
+const NODE_DIM = '\x1b[2m';
+
+const isNode =
+  typeof process !== 'undefined' &&
+  process.versions != null &&
+  process.versions.node != null;
+
+const isBrowserDarkTheme =
+  !isNode &&
+  typeof window.matchMedia === 'function' &&
+  window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+function createColor(config = {}) {
+  const {backgroundColor = '', weight = ''} = config;
+  let {color = ''} = config;
+
+  function print(string) {
+    if (isNode) {
+      console.log(`${backgroundColor}${color}${weight}${string}${NODE_RESET}`);
+    } else {
+      if (!color) {
+        color = isBrowserDarkTheme ? [255, 255, 255] : [0, 0, 0];
+      }
+
+      let backgroundColorStyle = '';
+      if (backgroundColor) {
+        backgroundColorStyle = `background-color: rgb(${backgroundColor.join(
+          ',',
+        )});`;
+      }
+
+      let colorStyle = '';
+      if (weight === NODE_DIM) {
+        colorStyle = `color: rgba(${color.join(',')},0.5);`;
+      } else {
+        colorStyle = `color: rgb(${color.join(',')});`;
+      }
+
+      let weightStyle = '';
+      if (weight === NODE_BOLD) {
+        weightStyle = 'font-weight: bold;';
+      }
+
+      console.log(
+        '%c%s',
+        `${backgroundColorStyle}${colorStyle}${weightStyle}`,
+        string,
+      );
+    }
+  }
+
+  for (const key in COLORS.background) {
+    Object.defineProperty(print, key, {
+      get: () =>
+        createColor({
+          ...config,
+          backgroundColor: isNode
+            ? COLORS.background[key].node
+            : COLORS.background[key].browser,
+        }),
+    });
+  }
+
+  for (const key in COLORS.foreground) {
+    Object.defineProperty(print, key, {
+      get: () =>
+        createColor({
+          ...config,
+          color: isNode
+            ? COLORS.foreground[key].node
+            : COLORS.foreground[key].browser,
+        }),
+    });
+  }
+
+  Object.defineProperty(print, 'bold', {
+    get: () =>
+      createColor({
+        ...config,
+        weight: NODE_BOLD,
+      }),
+  });
+
+  Object.defineProperty(print, 'dim', {
+    get: () =>
+      createColor({
+        ...config,
+        weight: NODE_DIM,
+      }),
+  });
+
+  return print;
+}
+
+const baseConfig = createColor();
+
+export const {
+  bold,
+  dim,
+
+  bgBlack,
+  bgRed,
+  bgGreen,
+  bgYellow,
+  bgBlue,
+  bgMagenta,
+  bgCyan,
+  bgWhite,
+
+  black,
+  red,
+  green,
+  yellow,
+  blue,
+  magenta,
+  cyan,
+  white,
+} = baseConfig;
